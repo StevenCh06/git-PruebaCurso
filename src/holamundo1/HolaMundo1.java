e7dce828b537d5909c0a99c8fd58e23ed9181014
Steven <stchch06@gmail.com>
2022-11-09 21:55:03 -0600

    mi primer commit

---
+++ src/holamundo1/HolaMundo1.java
@@ -0,0 +1,31 @@
+
+package holamundo1;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class HolaMundo1 {
+
+    public static void main(String[] args) 
+    {
+        Scanner lectura = new Scanner (System.in);
+
+        System.out.println("Ingrese el segundo número: ");
+
+        int num2 = lectura.nextInt();
+
+        System.out.println("Ingrese el primer número: ");
+
+        int num1 = lectura.nextInt();
+        
+        
+        
+        System.out.println("");
+
+        //System.out.println("Su nombre es: " + nombre + " y su edad es: " + edad);
+        
+        
+        
+    }
+    
+}
